98be485e5441a0ba4efbcd8b0fa46749e67c423f
Your Name <your.email@example.com>
2025-07-22 13:31:13 +0530

    add url

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: "http://localhost:8000",
+    API_URL: "https://annapurna-i54c.vercel.app",
     GOOGLE_CLIENT_ID: "37607736898-g23m11gjlelgeenj67t1d2oh5m8bv9ae.apps.googleusercontent.com"
 }
 // https://annapurna-i54c.vercel.app
